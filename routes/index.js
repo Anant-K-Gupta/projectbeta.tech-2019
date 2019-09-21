6693be3365f3a64b3595c510d02cd694075fabfe
KrishSkywalker <49684198+KrishSkywalker@users.noreply.github.com>
2019-09-21 23:19:15 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ router.get('/', (req, res, next) => {
     return res.render('index');
 });
 
-['leaderboard', 'betatest', 'home', 'team', 'events', 'alumni', 'contact', '404'].map(page => {
+['leaderboard', 'betatest', 'home', 'team', 'events', 'alumni', 'contact', '404', 'register'].map(page => {
     router.get(`/${page}`, (req, res, next) => {
         return res.redirect(`./#/${page}`);
     })
