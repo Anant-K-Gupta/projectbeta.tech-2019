c2a0aac1df09d4571c2a0105bc81510574928896
KrishSkywalker <49684198+KrishSkywalker@users.noreply.github.com>
2019-09-23 20:36:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const subdomain = require('express-subdomain');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
 const app = express();
+
+app.use(cookieParser())
+app.use(session({
+    secret: 'MuleILoveYou',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000
+    }
+}))
 
 //Setting up body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+    extended: true
 }));
 
 //Setting public directory
@@ -18,4 +33,18 @@ app.set('views', __dirname + '/views/pages');
 const home = require('./routes/index');
 app.use('/', home);
 
-app.listen(process.env.PORT || 2500);
+const register = require('./routes/register');
+app.use('/register', register);
+
+//404
+app.use((res, req, next) => {
+    next(err);
+});
+
+//Error Handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.redirect('./#/404');
+});
+
+app.listen(process.env.PORT || 5000);
